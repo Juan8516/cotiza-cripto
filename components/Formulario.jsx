76649d2fa18fe820004dd00d5520369db931ec56
Juan8516 <juan.ind85@gmail.com>
2023-10-19 16:03:50 -0500

    Se aplica estilos al label y select elige tu moneda y select

--- components/Formulario.jsx
+++ components/Formulario.jsx
@@ -13,7 +13,8 @@ const InputSubmit = styled.input`
     text-transform: uppercase;
     font-size: 20px;
     border-radius: 5px;
-    transition: background-color .2s ease;
+    transition: background-color .3s ease;
+    margin-top: 30px;
     &:hover {
         background-color: #7A7DFE;
         cursor: pointer;
@@ -21,8 +22,6 @@ const InputSubmit = styled.input`
 `
 
 const Formulario = () => {
-
-
 
   const [ SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas)
 
--- hooks/useSelectMonedas.jsx
+++ hooks/useSelectMonedas.jsx
@@ -3,6 +3,18 @@ import styled from '@emotion/styled'
 
 const Label = styled.label`
     color: #fff;
+    display: block;
+    font-family: 'Lato', sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    margin: 15px 0;
+`
+
+const Select = styled.select`
+    width: 100%;
+    font-size: 18px;
+    padding: 10px;
+    border-radius: 10px;
 `
 
 const useSelectMonedas = (label, opciones) => {
@@ -11,7 +23,7 @@ const useSelectMonedas = (label, opciones) => {
         <>
             <Label>{label}</Label>
             
-            <select>
+            <Select>
                 <option value="">Seleccione</option>
 
                 {opciones.map( opcion => (
@@ -20,7 +32,7 @@ const useSelectMonedas = (label, opciones) => {
                         value={opcion.id}
                     >{opcion.nombre}</option>
                 ))}
-            </select>
+            </Select>
         </>
     )
 
