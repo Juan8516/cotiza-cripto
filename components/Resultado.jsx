1d8ce7f63d33231b37cc8d2052de91612eadc8ba
Juan8516 <juan.ind85@gmail.com>
2023-10-23 07:07:22 -0500

    Destructuracion de valores, para mostrar resultados

--- components/Resultado.jsx
+++ components/Resultado.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Resultado = () => {
+const Resultado = ( {resultado} ) => {
+    const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } = resultado
   return (
     <div>
-      <h1>Desde Resultado ... </h1>
+      <p>El precio es: <span>{PRICE}</span></p>
+      <p>El precio mas alto del dia es: <span>{HIGHDAY}</span></p>
+      <p>El precio mas bajo del dia es: <span>{LOWDAY}</span></p>
+      <p>El precio ultimas 24HRS: <span>{CHANGEPCT24HOUR}</span></p>
+      <p>Ultima actualizacion: <span>{LASTUPDATE}</span></p>
     </div>
   )
 }
