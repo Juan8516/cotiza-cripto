222a96c724e2ad7d530408300484647ab2c35d13
Juan8516 <juan.ind85@gmail.com>
2023-10-23 08:31:20 -0500

    Implementacion estilos a resultados de buesqueda

--- components/Resultado.jsx
+++ components/Resultado.jsx
@@ -1,15 +1,41 @@
 import React from 'react'
+import styled from '@emotion/styled'
+
+const Contenedor = styled.div`
+    color: #fff;
+    font-family: 'Lato', sans-serif;
+    text-align: center;
+
+`
+
+const Texto = styled.p`
+    font-size: 18px;
+    text-align: start;
+    span {
+        font-weight: 700;
+        color: #D4AF37;
+    }
+`
+
+const Precio = styled.p`
+    font-size: 30px;
+    span {
+        font-weight: 700;
+        color: #D4AF37;
+    }
+`
+
 
 const Resultado = ( {resultado} ) => {
     const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } = resultado
   return (
-    <div>
-      <p>El precio es: <span>{PRICE}</span></p>
-      <p>El precio mas alto del dia es: <span>{HIGHDAY}</span></p>
-      <p>El precio mas bajo del dia es: <span>{LOWDAY}</span></p>
-      <p>El precio ultimas 24HRS: <span>{CHANGEPCT24HOUR}</span></p>
-      <p>Ultima actualizacion: <span>{LASTUPDATE}</span></p>
-    </div>
+    <Contenedor>
+      <Precio>El precio es: <span>{PRICE}</span></Precio>
+      <Texto>El precio mas alto del dia es: <span>{HIGHDAY}</span></Texto>
+      <Texto>El precio mas bajo del dia es: <span>{LOWDAY}</span></Texto>
+      <Texto>El precio ultimas 24HRS: <span>{CHANGEPCT24HOUR}</span></Texto>
+      <Texto>Ultima actualizacion: <span>{LASTUPDATE}</span></Texto>
+    </Contenedor>
   )
 }
 
