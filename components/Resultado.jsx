3e0a09153daba30d9497c83137630b2cb46780fd
Juan8516 <juan.ind85@gmail.com>
2023-10-23 09:22:06 -0500

    Se asocian imagenes a criptmonedas

--- components/Resultado.jsx
+++ components/Resultado.jsx
@@ -4,21 +4,30 @@ import styled from '@emotion/styled'
 const Contenedor = styled.div`
     color: #fff;
     font-family: 'Lato', sans-serif;
-    text-align: center;
 
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-top: 30px;
+`
+
+const Imagen = styled.img`
+    display: block;
+    width: 120px;
 `
 
 const Texto = styled.p`
     font-size: 18px;
     text-align: start;
     span {
-        font-weight: 700;
+        font-weight: 400;
+        font-size: medium;
         color: #D4AF37;
     }
 `
 
 const Precio = styled.p`
-    font-size: 30px;
+    font-size: 24px;
     span {
         font-weight: 700;
         color: #D4AF37;
@@ -30,11 +39,17 @@ const Resultado = ( {resultado} ) => {
     const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } = resultado
   return (
     <Contenedor>
-      <Precio>El precio es: <span>{PRICE}</span></Precio>
-      <Texto>El precio mas alto del dia es: <span>{HIGHDAY}</span></Texto>
-      <Texto>El precio mas bajo del dia es: <span>{LOWDAY}</span></Texto>
-      <Texto>El precio ultimas 24HRS: <span>{CHANGEPCT24HOUR}</span></Texto>
-      <Texto>Ultima actualizacion: <span>{LASTUPDATE}</span></Texto>
+        <Imagen 
+            src={`https://www.cryptocompare.com/${IMAGEURL}`} 
+            alt='imagen cripto'
+        />
+        <div>
+            <Precio>El precio es: <span>{PRICE}</span></Precio>
+            <Texto>El precio + alto del dia: <span>{HIGHDAY}</span></Texto>
+            <Texto>El precio + bajo del dia: <span>{LOWDAY}</span></Texto>
+            <Texto>Variacion ultimas 24HRS: <span>${CHANGEPCT24HOUR}</span></Texto>
+            <Texto>Ultima actualizacion: <span>{LASTUPDATE}</span></Texto>
+        </div>
     </Contenedor>
   )
 }
